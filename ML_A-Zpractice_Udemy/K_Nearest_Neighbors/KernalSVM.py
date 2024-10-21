40b108d59fe243ebbbec24dfdf3fffde8d25d598
Venkatesh Nagandla <43311042+venqics@users.noreply.github.com>
2024-10-21 06:42:46 +0530

    Exercise files from each Machine learning Concept

---
+++ ML_A-Zpractice_Udemy/K_Nearest_Neighbors/KernalSVM.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 02:22:03 2019
+
+@author: venxr
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+#Fit the classifier to training set
+
+from sklearn.svm import SVC
+classifier = SVC()
